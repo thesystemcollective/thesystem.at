14f9eab1c84c4708f187cdcf8209eee792e5c3d3
jaeh <jascha@jaeh.at>
2021-02-08 14:55:29 +0100

    Partners: update image sizes and scaling

--- src/assets/Partners.mjs
+++ src/assets/Partners.mjs
@@ -1,47 +1,51 @@
 export const View = (_, children) =>
   div({ class: 'Partners' }, [
-    h3('Commissioned by:'),
+    div({ class: 'Commissioned' }, [
+      h3('Commissioned by:'),
 
-    Link({ title: 'commissioned by kör', to: 'https://koer.or.at' }, [
-      Img({
-        class: 'koer',
-        alt: 'kör',
-        src: 'https://static.artificialmuseum.com/support/koer.svg',
-        width: '255',
-        height: '100',
-      }),
-      Img({
-        class: 'koer',
-        alt: 'wien kultur',
-        src: 'https://static.artificialmuseum.com/support/wien-kultur.svg',
-        width: '242',
-        height: '100',
-      }),
+      Link({ title: 'commissioned by kör', to: 'https://koer.or.at' }, [
+        Img({
+          class: 'koer',
+          alt: 'kör',
+          src: 'https://static.artificialmuseum.com/support/koer.svg',
+          width: '255',
+          height: '100',
+        }),
+        Img({
+          class: 'koer',
+          alt: 'wien kultur',
+          src: 'https://static.artificialmuseum.com/support/wien-kultur.svg',
+          width: '242',
+          height: '100',
+        }),
+      ]),
     ]),
 
-    h3('Support:'),
+    div({ class: 'Support' }, [
+      h3('Support:'),
 
-    Link(
-      { to: 'https://usus.wien' },
-      Img({
-        title: 'yppshop provided by usus.wien',
-        src: 'https://static.artificialmuseum.com/support/usus.svg',
-        width: '80',
-        height: '35',
-      }),
-    ),
+      Link(
+        { to: 'https://usus.wien' },
+        Img({
+          title: 'yppshop provided by usus.wien',
+          src: 'https://static.artificialmuseum.com/support/usus.svg',
+          width: '80',
+          height: '35',
+        }),
+      ),
 
-    Link(
-      { to: 'https://magic.github.io' },
-      Img({
-        title: 'made with a few bits of magic',
-        src: 'https://static.artificialmuseum.com/support/magic.svg',
-        width: '40',
-        height: '35',
-      }),
-    ),
+      Link(
+        { to: 'https://magic.github.io' },
+        Img({
+          title: 'made with a few bits of magic',
+          src: 'https://static.artificialmuseum.com/support/magic.svg',
+          width: '40',
+          height: '35',
+        }),
+      ),
 
-    children,
+      children,
+    ]),
   ])
 
 export const style = vars => ({
@@ -56,9 +60,17 @@ export const style = vars => ({
 
   img: {
     height: 'auto',
+    width: 'auto',
     margin: '0 2em 0 0',
     maxWidth: '90vw',
     maxHeight: '62px',
+  },
+
+  '.Support': {
+    img: {
+      maxHeight: '24px',
+      margin: '0 0.5em 0 0',
+    },
   },
 
   '.koer': {

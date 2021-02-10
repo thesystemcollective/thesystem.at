a7335d49c096c7fa9745796b7c6c9fd73afcb605
jaeh <jascha@jaeh.at>
2021-02-10 22:12:30 +0100

    add Agenda 2021 placeholder

---
+++ src/assets/Agenda2021.mjs
@@ -0,0 +1,37 @@
+export const View = () => [
+  h2('Agenda 2021'),
+
+  h3('February: Opening'),
+  p('We opened the Artificial Museum on the 1st of February.'),
+
+  h3('No Walls'),
+  p(
+    'The exhibition "No Walls" aims to create a decentralized, localized exhibition all over Vienna and other cities.',
+  ),
+  p(
+    'This includes multiple exhibitions, interventions and collaborations that will get executed over the next year.',
+  ),
+  p(
+    'We will focus on all areas of life in which our freedoms are restricted, on retracing of power structures and on dissolving them.',
+  ),
+
+  h3('No Walls For Artists'),
+  p(
+    'Starting in may, we will feature artists and their artworks. Featured artworks can be positioned anywhere.',
+  ),
+
+  h3('No Walls for Education / Science'),
+  p(
+    'We will collaborate with institutions, exhibitions and collectives, working together to represent our shared visions.',
+  ),
+
+  h3('No Walls for Museums'),
+  p([
+    'Beginning in October ("Lange Nacht der Museen", "Tag des Museums") and running until November 9th ("Visit an Art Museum day"),',
+    ' "No Walls for Museums" will position artworks around austrian museums and close to art exhibitions, institutions and other art and culture related places.',
+    ' These artworks will be a permanent commemoration to those important places of culture and history.',
+  ]),
+
+  h3('No Walls for …'),
+  p('We are planning interventions and exhibitions throughout the year.'),
+]

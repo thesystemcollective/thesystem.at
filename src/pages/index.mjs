62873a93d2ec60bcaf9aaa6e12cae36b7f4ca3a3
jaeh <jascha@jaeh.at>
2021-02-10 22:15:22 +0100

    src/pages/index.mjs: add state.logotext, update content

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -1,69 +1,53 @@
-export const View = ({ artists, collectives }) => [
-  PageHeader('Collective'),
+export const state = {
+  logotext: 'Collective',
+}
 
+export const View = ({ artists, collectives }) => [
   div({ class: 'content' }, [
+    Picture({ src: '/img/hero', type: 'jpg', height: 707, width: 1000 }),
+
     div({ class: 'manifest' }, [
-      p([
-        i('TheSystemCollective'),
-        ' is a syndicate of independent',
-        ' artists, makers, programmers, engineers, philosophers, activists, and scientists,',
-        ' collectively using art and technology to transform our world.',
-        ' We believe in a literal definition of "common goods",',
-        ' and convert public spaces into a museum,',
-        ' ready for artists to freely position their art.',
+      div({ class: 'Slogan' }, [
+        "We don't need space, time is irrelevant, objects stay as useful as the user.",
       ]),
 
-      p('Our method is an intermixture of universes, physical, virtual and imaginary.'),
+      p([
+        'We are ',
+        i('TheSystemCollective'),
+        ', a syndicate of independent',
+        ' artists, makers, programmers, engineers, philosophers, activists, and scientists,',
+        ' collectively using art and technology to transform our world.',
+      ]),
+      p([
+        'We believe in a literal definition of “common goods”',
+        ' and transform public space into a bearer of art,',
+        ' turning those works of art into an integral part of urban culture.',
+      ]),
 
-      p(i("We don't need space, time is irrelevant, objects stay as useful as the user.")),
+      Picture({ src: '/img/artificialmuseum_map', type: 'jpg', height: 500, width: 1000 }),
+
+      div({ class: 'Slogan' }, 'We are timeless, have unlimited space and are always open to the public.'),
 
       p([
-        'Our main purpose is the creation, procurement, research, dissemination and conservation of (digital) art in the ',
+        'The cultural mission of ',
+        i('TheSystemCollective'),
+        ' is the creation, procurement, research, dissemination and conservation of (digital) art for the ',
         b(Link({ to: 'https://artificialmuseum.com/', text: 'Artificial Museum' })),
         ', a time space continuum made ',
         b('manifest'),
-        ', created to  make art accessible to everyone, both at home and abroad.',
+        ', created to make art accessible to everyone, both at home and abroad.',
       ]),
 
-      h2('Agenda 2021'),
+      p('The intermixture of virtual, imaginary worlds with reality is our method, allowing works of art to freely be placed in public spaces.'),
 
-      h3('February: Opening'),
-      p('We opened the Artificial Museum on the 1st of February.'),
+      p(i('“Art is not the way to more democracy and equality, but art can be the impetus to think.”')),
 
-      h3('No Walls'),
-      p(
-        'The exhibition "No Walls" aims to create a decentralized, localized exhibition all over Vienna and other cities.',
-      ),
-      p(
-        'This includes multiple exhibitions, interventions and collaborations that will get executed over the next year.',
-      ),
-      p(
-        'We will focus on all areas of life in which our freedoms are restricted, on retracing of power structures and on dissolving them.',
-      ),
+      Picture({ src: '/img/artificialmuseum_preview_litto_hrlitto', type: 'jpg', height: 360, width: 1000 }),
 
-      h3('No Walls For Artists'),
-      p(
-        'Starting in may, we will feature artists and their artworks. Featured artworks can be positioned anywhere.',
-      ),
+      Picture({ src: '/img/artificialmuseum_preview_zirkusmops_metzgereimops', type: 'jpg', height: 360, width: 1000 }),
+      Picture({ src: '/img/artificialmuseum_preview_stefansturzer_systemsandhumans', type: 'jpg', height: 360, width: 1000 }),
 
-      h3('No Walls for Education / Science'),
-      p(
-        'We will collaborate with institutions, exhibitions and collectives, working together to represent our shared visions.',
-      ),
-
-      h3('No Walls for Museums'),
-      p([
-        'Beginning in October ("Lange Nacht der Museen", "Tag des Museums") and running until November 9th ("Visit an Art Museum day"),',
-        ' "No Walls for Museums" will position artworks around austrian museums and close to art exhibitions, institutions and other art and culture related places.',
-        ' These artworks will be a permanent commemoration to those important places of culture and history.',
-      ]),
-
-      h3('No Walls for …'),
-      p('We are planning interventions and exhibitions throughout the year.'),
-
-      List({ list: collectives, title: 'Involved Collectives' }),
-
-      List({ list: artists, title: 'Represented Artists' }),
+      // Agenda2021(),
     ]),
 
     Partners(),

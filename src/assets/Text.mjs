e2dbab37b405ea700ff69dc2064818607035948e
jaeh <jascha@jaeh.at>
2021-02-11 21:18:54 +0100

    Text: remove width and padding

--- src/assets/Text.mjs
+++ src/assets/Text.mjs
@@ -6,8 +6,6 @@ export const style = v => ({
   margin: '0 auto 3em',
 
   [`@media screen and (min-width: ${v.widths.laptop})`]: {
-    width: '70vw',
     margin: '0 auto 6em',
-    padding: '6em 1em 0',
   },
 })

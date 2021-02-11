481e7687fa8ac15adcc3c72a1c343302f0e0bf39
jaeh <jascha@jaeh.at>
2021-02-11 03:51:16 +0100

    Picture: images float left-right alternating

--- src/assets/Picture.mjs
+++ src/assets/Picture.mjs
@@ -1,6 +1,22 @@
-export const View = ({ src, type = 'jpg', alt, width, height }) => [
-  picture([
+export const View = ({ class: cl, src, type = 'jpg', alt, width, height }) => [
+  picture({ class: 'Picture' }, [
     source({ type: 'image/webp', srcset: `${src}.webp` }),
     Img({ src: `${src}.${type}`, alt, width, height }),
   ]),
 ]
+
+export const style = {
+  display: 'inline-block',
+  width: '100%',
+
+  img: {
+    maxWidth: '95%',
+    float: 'right',
+  },
+
+  '&:nth-of-type(2n)': {
+    img: {
+      float: 'left',
+    },
+  },
+}

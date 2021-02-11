69c378212e0d76777adb3cd8ccb94941353a2aff
jaeh <jascha@jaeh.at>
2021-02-11 23:25:56 +0100

    Text: no top and bottom margin

--- src/assets/Text.mjs
+++ src/assets/Text.mjs
@@ -2,8 +2,8 @@ export const View = children => div({ class: 'Text' }, children)
 
 export const style = v => ({
   clear: 'both',
-  padding: '1em 1em 0',
-  margin: '0 auto 3em',
+  padding: '0 1em 0',
+  margin: '0 auto',
 
   [`@media screen and (min-width: ${v.widths.laptop})`]: {
     margin: '0 auto 6em',

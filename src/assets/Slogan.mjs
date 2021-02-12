1f9e34f203a90dee770b4b6613ac5b4783ccba18
jaeh <jascha@jaeh.at>
2021-02-12 16:28:45 +0100

    Slogan: letter-spacing, maxWidth

--- src/assets/Slogan.mjs
+++ src/assets/Slogan.mjs
@@ -2,7 +2,8 @@ export const View = children => div({ class: 'Slogan' }, children)
 
 export const style = {
   fontSize: '2em',
-  letterSpacing: '-4px',
+  letterSpacing: '-3px',
   fontWeight: 500,
   margin: '0 0 1em',
+  maxWidth: '800px',
 }

15d84ef48aa5ca8eb17c808d9db58a2c64f191b7
jaeh <jascha@jaeh.at>
2021-02-12 01:08:09 +0100

    Text: remove media query

--- src/assets/Text.mjs
+++ src/assets/Text.mjs
@@ -4,8 +4,4 @@ export const style = v => ({
   clear: 'both',
   padding: '0 1em 0',
   margin: '0 auto',
-
-  [`@media screen and (min-width: ${v.widths.laptop})`]: {
-    margin: '0 auto 6em',
-  },
 })

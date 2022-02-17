3232eb2df7271c7e13fe509779df1eddf5d3bb1e
jascha <j@jaeh.at>
2022-02-18 00:04:05 +0100

    modules/Header: add Branding div wrapper, update styles

--- src/assets/modules/Header.js
+++ src/assets/modules/Header.js
@@ -4,6 +4,7 @@ export const View = props => {
   const { menu, hash, url } = props
 
   return div({ class: 'Header' }, [
+    div({ class: 'Branding' }, [
     div(
       { class: 'Logo' },
       svg({ class: 'logo', viewBox: '0 0 54 60', width: '60', height: '54' }, [
@@ -13,17 +14,38 @@ export const View = props => {
     ),
 
     div({ class: 'Slogan' }, [p(b('hello world')), p('we are the SystemKollektiv')]),
+    ]),
 
     menu && Menu({ url, hash, menu }),
   ])
 }
 
-export const style = {
+export const style = vars => ({
   width: '100%',
+
+  '.Branding': {
+    display: 'inline-block',
+  },
 
   '.Logo': {
     float: 'left',
     height: '2em',
+    margin: '0 0.3em 0 0',
+
+    svg: {
+      maxHeight: '100%',
+      maxWidth: '100%',
+      height: 'auto',
+      width: 'auto',
+    },
+  },
+
+  '.Slogan': {
+    float: 'left',
+
+    p: {
+      lineHeight: 1,
+    },
   },
 
   '.Menu': {
@@ -31,7 +53,16 @@ export const style = {
     textAlign: 'center',
     margin: '0.5em 0 1em',
   },
-}
+
+  [`@media screen and (min-width: ${vars.widths.laptop})`]: {
+    '.Branding': {
+      float: 'left',
+    },
+    '.Menu': {
+      float: 'right',
+    },
+  },
+})
 
 export const propTypes = {
   Header: [
